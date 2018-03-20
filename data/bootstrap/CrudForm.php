81743ae0d06b75d9942c1e6124395bb2a0dc62ec
izosa <izosa@msn.com>
2018-03-20 11:12:25 +0200

    = CrudForm extends kartikform add checkbox, add content

--- data/bootstrap/CrudForm.php
+++ data/bootstrap/CrudForm.php
@@ -2,10 +2,10 @@
 namespace izosa\serengeti\data\bootstrap;
 
 use Yii;
-use yii\bootstrap\ActiveForm;
-use yii\helpers\Html;
-use letyii\tinymce\Tinymce;
 use kartik\icons\Icon;
+use \kartik\helpers\Html;
+use \kartik\form\ActiveForm;
+use letyii\tinymce\Tinymce;
 
 /**
  * Form implement boostrtap design
@@ -44,30 +44,72 @@ class CrudForm extends ActiveForm{
         $class = basename(Yii::$app->getBasePath()).'\assets\AppAsset';
         $class::register(Yii::$app->view);
 
-        return $this->field($model, $attribute)->widget(Tinymce::className(), [
-            'options' => $options,
+        return $this->field($model, $attribute, ['horizontalCssClasses' => ['wrapper' => 'col-sm-10']])->widget(Tinymce::className(), [
+            'options' => [
+                'id' => 'content',
+                'rows' => 20,
+                'required',
+
+            ],
             'configs' => [ // Read more: http://www.tinymce.com/wiki.php/Configuration
-                'plugins' => 'fullscreen,image,link,preview, visualblocks,code,table,print,media,lists',
+
+                // global
+                'width' => '750px',
+                'height' => '500px',
+                'max_width' => '750px',
+                'plugin_preview_width' => '750px',
+                'plugins' => 'lists,fullscreen,image,imagetools,link,preview, visualblocks,code,table,print,media,searchreplace,charmap,contextmenu',
                 'toolbar' => [
                     "preview fullscreen | undo redo |  alignleft aligncenter alignright alignjustify | indent outdent bullist numlist |"
-                    . " styleselect | bold italic underline blockquote removeformat | link image media | code visualblocks"
+                    . " styleselect || bold italic underline blockquote removeformat | link  image  media  table | code visualblocks  ",
                 ],
-                'relative_urls'=> false,
+
+                // url
+                'relative_urls' => false,
                 'remove_script_host' => false,
+
+                // todo
                 'document_base_url' => Yii::$app->request->hostInfo.dirname(Yii::$app->request->scriptUrl).'/',
 //                'language' => substr(Yii::$app->language,0,2),
 //                "language_url" => Yii::$app->assetManager->bundles['backend\assets\AppAsset']->baseUrl.'/js/lib/tinymce/langs/'.substr(Yii::$app->language,0,2).'.js',
                 'external_plugins' => [
 //                    "jbimages" => Yii::$app->assetManager->bundles['backend\assets\AppAsset']->baseUrl."/js/lib/tinymce/jbimages/plugin.min.js",
                 ],
+
+                // image
                 'image_advtab' => true,
-                'height' => '600',
                 'image_class_list' => [
                     ['title' => 'Responsive', 'value' => ''],
                     ['title' => 'Fixed', 'value' => 'imageFixed'],
                     ['title' => 'Align left', 'value' => 'imageLeft'],
-                    ['title' => 'Align right', 'value' => 'imageRight']
+                    ['title' => 'Align right', 'value' => 'imageRight'],
                 ],
+                'image_dimensions' => true,
+
+                // table
+                "table_class_list" => [
+                    ["title" => 'None', "value" => 'table table-striped'],
+                    ["title" => 'Small', "value" => 'table table-striped table-condensed'],
+                    ["title" => 'No Striped', "value" => 'table'],
+                    ["title" => 'No Striped Small', "value" => 'table table-condensed'],
+                ],
+                "table_default_attributes" => [
+                    'class' => 'table table-striped',
+                    'border' => 0,
+                    'style' => ''
+                ],
+                "invalid_styles" => [
+                    "table" => "width height border-collapse",
+                    "img" => "font-size, font-color font-style width height"
+                ],
+                "table_grid" => false,
+
+                // not filter scripts
+                'extended_valid_elements' => 'script[height|width|charset|defer|language|src|type]',
+
+                // live preview styles
+                'content_css' => Yii::$app->request->hostInfo . "/preview-styles.css",
+                'body_class' => 'articleBody itemContent',
             ],
         ]);
     }
@@ -75,20 +117,11 @@ class CrudForm extends ActiveForm{
 
     public function checkbox($model, $attribute, $options = [])
     {
-        return '<label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect" for="'.Html::getInputId($model, $attribute).'">   
-                '.Html::activeInput('checkbox',$model,$attribute, ['class' => 'mdl-checkbox__input' ]).'
-                <span class="mdl-checkbox__label">'.$model->getAttributeLabel($attribute).'</span>
-            </label>';
-    }
+        return $this->field($model, $attribute,[
+            'horizontalCheckboxTemplate' => "<div class=\"col-xs-offset-2 col-sm-10\">\n<div class=\"checkbox checkbox-success\">\n{input}\n{beginLabel}\n{labelTitle}\n{endLabel}\n</div>\n{error}\n</div>\n{hint}",
+        ])->checkbox($options,true);
 
-    public function switchbox($model, $attribute, $options = [])
-    {
-        return '<label class="mdl-switch mdl-js-switch mdl-js-ripple-effect" for="'.Html::getInputId($model, $attribute).'">   
-                '.Html::activeInput('checkbox',$model,$attribute, ['class' => 'mdl-switch__input' ]).'
-                <span class="mdl-switch__label">'.$model->getAttributeLabel($attribute).'</span>
-            </label>';
     }
-
 
     // buttons
 
